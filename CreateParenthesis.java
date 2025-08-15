a5025872571a9ebca4e9369df43f7bc7b1bff824
Rohit Kumar <89602368+Rohitanuj@users.noreply.github.com>
2025-08-15 23:20:26 +0530

    Create CreateParenthesis.java

---
+++ CreateParenthesis.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<String> generateParenthesis1(int n) {
+        List<String> anuj=new ArrayList<String>();
+        Backtraking("",anuj,n,n);
+        return anuj;
+    }
+public void Backtraking(String ans,List an,int left,int right){
+    if(right==0)
+    an.add(ans);
+
+    if(left<right)
+    Backtraking(ans+')',an,left,right-1);
+
+    if(left> 0){
+    Backtraking(ans+'(',an,left-1,right);
+    }
+}
+}
